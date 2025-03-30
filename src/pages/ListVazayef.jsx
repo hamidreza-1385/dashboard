6dfcee11b47804363c13edfb47a0bf54ad9ac51e
hamidreza <g4531232@gmail.com>
2025-03-30 11:24:50 +0330

    4

--- src/pages/ListVazayef.jsx
+++ src/pages/ListVazayef.jsx
@@ -1,16 +1,34 @@
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
+import TaskForm from '../components/VazayefV/TaskForm';
+import TaskList from '../components/VazayefV/TaskList';
+import { useState } from 'react';
 const ListVazayef = () => {
+    const [tasks, setTasks] = useState([]);
+  
+    const addTask = (text) => {
+      setTasks([...tasks, { id: Date.now(), text, completed: false }]);
+    };
+  
+    const toggleTask = (id) => {
+      setTasks(
+        tasks.map((task) =>
+          task.id === id ? { ...task, completed: !task.completed } : task
+        )
+      );
+    };
+  
     return (
         <div className="dashboard-container ml">
-            <Sidebar />
-            <div className="main-content">
-                <Header />
-                ListVazayef
-                
+        <Sidebar />
+        <div className="main-content">
+            <Header />
+            <div className='bg-white rounded-4 p-4 justify-content-end'>
+                list vazayef
             </div>
         </div>
+    </div>
     );
-};
-
-export default ListVazayef;
+  };
+  
+  export default ListVazayef;
